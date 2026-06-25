bb281c677f1532597fd389d5e4ba1ab720fe8ea9
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2026-06-25 23:14:25 +0300

    create getOrCreateUser function

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,8 +1,17 @@
 import AppShell from "@/components/layout/AppShell";
-import { Show } from "@clerk/nextjs";
-// import Header fro@/components/ui/PageHeaderder";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { getOrCreateUser } from "@/lib/users";
 
-export default function Layout({ children }) {
+export default async function Layout({ children }) {
+
+  const { userId } = await auth()
+  const clerkUser = await currentUser()
+
+  const email = clerkUser.emailAddresses[0].emailAddress;
+  const name = clerkUser.firstName
+
+  const user = await getOrCreateUser(userId, email, name)
+
   return (
     <AppShell>
       <div className="sm:px-10 px-4 sm:py-8 py-6">
